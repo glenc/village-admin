8c006909ab14abbe8da635ee3d1c411d1362f006
Glen Cooper <glenc@pico-logic.com>
2013-07-31 22:28:15 -0700

    Added some simple dashboard stats

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('villageAdminApp')
-  .controller('DashboardCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('DashboardCtrl', function ($scope, villageApi) {
+    villageApi.family.query(function(results) {
+      $scope.stats = {
+        families: results.length,
+        students: _.reduce(results, function(total,f) { return f.students.length + total;}, 0),
+        contacts: _.reduce(results, function(total,f) { return f.contacts.length + total;}, 0)
+      }
+    });
   });
