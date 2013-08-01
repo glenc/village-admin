29c8b1cb7da466eb8b69d0a10be1000680a193e7
Glen Cooper <glenc@pico-logic.com>
2013-07-31 22:33:07 -0700

    Create new families from dashboard

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('villageAdminApp')
-  .controller('DashboardCtrl', function ($scope, villageApi) {
+  .controller('DashboardCtrl', function ($scope, $location, villageApi) {
     villageApi.family.query(function(results) {
       $scope.stats = {
         families: results.length,
@@ -9,4 +9,15 @@ angular.module('villageAdminApp')
         contacts: _.reduce(results, function(total,f) { return f.contacts.length + total;}, 0)
       }
     });
+
+    $scope.newFamily = {
+      name: ''
+    };
+    $scope.createFamily = function() {
+      villageApi.family.save({name:$scope.newFamily.name}, function(result) {
+        if (result) {
+          $location.path("/families/" + result._id);
+        }
+      })
+    }
   });
