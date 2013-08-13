837ab77ba1013635cb22a14f5bdd0aec420f2153
Glen Cooper <glenc@pico-logic.com>
2013-08-12 20:09:26 -0700

    Edit student dialog loads classes from api

--- app/scripts/controllers/edit-student.js
+++ app/scripts/controllers/edit-student.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('villageAdminApp')
-  .controller('EditStudentCtrl', function ($scope, dialog, item) {
+  .controller('EditStudentCtrl', function ($scope, villageApi, dialog, item) {
     $scope.title = item ? 'Edit Student' : 'New Student';
-    console.log(item);
     $scope.item = item || {};
+    $scope.classes = [ 'Please Wait...' ];
+
+    villageApi.config.query({query:'key:school-calendar'}, function(results) {
+      $scope.classes = Object.keys(results[0].value.classes);
+    });
+
     $scope.classes = ['2013', '2014', '2015', '2016', '2018'];
     $scope.submit = function() {
       dialog.close($scope.item);
