261a374b259141bd7909e17c541bea022794795b
Glen Cooper <glenc@pico-logic.com>
2013-08-31 10:41:49 -0700

    fixes #8 Corrected binding and stuff for delete button popup

--- app/scripts/directives/delete-button.js
+++ app/scripts/directives/delete-button.js
@@ -7,39 +7,29 @@ angular.module('villageAdminApp')
       replace: true,
       scope: {
         text: '@',
+        confirmText: '@',
         class: '@',
         action: '&'
       },
-      link: function(scope, elem, attrs) {
+      controller: function($scope, $element) {
         var popoverOpts = {
-          title: attrs.confirmText || 'Are you sure?',
+          title: $scope.confirmText || 'Are you sure?',
           placement: 'top',
           html: true,
           content: '<button class="btn btn-primary btn-small" data-result="yes"><i class="icon-ok"></i> Yes</button>'+
                    '<button class="btn btn-small" data-result="no"><i class="icon-ban-circle"></i> No</button>'
         };
 
-        var hidePopover = function() {
-          elem.popover('hide');
-        };
-
-        var handleResult = function(e) {
-          var el = $(e.target);
-          var result = el.data('result');
-          hidePopover();
-          if (result === 'yes') {
-            scope.action();
-          }
-        };
-
-        elem.popover(popoverOpts)
-            .parent()
-            .delegate('button', 'click', handleResult);
-
-        elem.bind('click', function(e) {
-          e.preventDefault();
-          elem.popover('show');
-        });
+        $element.popover(popoverOpts)
+          .parent()
+          .delegate('button', 'click', function(e) {
+            var el = $(e.currentTarget);
+            var result = el.data('result');
+            $element.popover('hide');
+            if (result === 'yes') {
+              $scope.$apply($scope.action);
+            }
+          });
       },
       template: '<a href="" class="btn btn-danger {{class}}"><i class="icon-remove"></i> {{text}}</a>'
     };
